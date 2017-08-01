dde14ec991cc236b6bdc7deca7415f0d814a8273
Yevhenii <zvezdnev@gmail.com>
2017-08-01 20:36:03 +0300

    upd

--- src/config/mor.php
+++ src/config/mor.php
@@ -4,10 +4,10 @@ return [
 	'url'		=>	'https://mor.url.com',
 
     // API USERNAME
-    'username'	=>	'admin_user',//'admin'
+    'username'	=>	'admin_user',
 
     // API PASSWORD
-    'password'	=>	'password123',//'zachesubaBA7ecRU'
+    'password'	=>	'password123',
 
     // API PROCESSOR
 	'processor' =>	'api2016.php',
